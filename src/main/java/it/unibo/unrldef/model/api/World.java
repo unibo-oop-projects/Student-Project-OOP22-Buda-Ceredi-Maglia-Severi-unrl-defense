a1520db552786863e758bb2f57ead5832e499bfb
Francesco Buda <francesco.buda3@studio.unibo.it>
2023-02-18 13:44:32 +0100

    added the API for a game world

---
+++ src/main/java/it/unibo/unrldef/model/api/World.java
@@ -0,0 +1,16 @@
+package it.unibo.unrldef.model.api;
+
+import java.util.List;
+import java.util.Set;
+
+import it.unibo.unrldef.common.*;
+
+public interface World {
+    void startWave(Wave wave);
+    void buildTower(Position pos, Tower tower);
+    List<GameObject> getSceneEntities();
+    CastleIntegrity getCastleIntegrity();
+    int getMoney();
+    Set<Tower> getTowers();
+    Path getPath();
+}
