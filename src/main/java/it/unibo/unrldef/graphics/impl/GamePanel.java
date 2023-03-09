91b0d10410ae776f5805aacbc18411b1a7f2827d
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2023-03-09 17:21:54 +0100

    First implementation of the rendering of spells

--- src/main/java/it/unibo/unrldef/graphics/impl/GamePanel.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/GamePanel.java
@@ -3,11 +3,15 @@ package it.unibo.unrldef.graphics.impl;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import it.unibo.unrldef.model.api.Enemy;
 import it.unibo.unrldef.model.api.Entity;
+import it.unibo.unrldef.model.api.Spell;
 import it.unibo.unrldef.model.api.World;
 import it.unibo.unrldef.model.impl.Goblin;
 import it.unibo.unrldef.model.impl.Orc;
@@ -19,11 +23,19 @@ public class GamePanel extends JPanel {
     private World gameWorld;
     private Image orcImage;
     private Image goblinImage;
+    private Image fireballImage;
+    private Image arrowsImage;
 
 
     public GamePanel(World gameWorld) {
         super();
         //TODO: load assets
+        try {
+            this.fireballImage = ImageIO.read(new File("assets\\fireball.png"));
+            this.arrowsImage = ImageIO.read(new File("assets\\arrows.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.gameWorld = gameWorld;
     }
 
@@ -31,7 +43,6 @@ public class GamePanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D graphic = (Graphics2D) g;
-
         graphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphic.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         graphic.clearRect(0,0,this.getWidth(),this.getHeight());
@@ -42,8 +53,10 @@ public class GamePanel extends JPanel {
     }
 
     private void renderEntity(Graphics2D graphic, Entity entity) {
-        if(entity instanceof Enemy) {
+        if (entity instanceof Enemy) {
             renderEnemy(graphic, entity);
+        } else if (entity instanceof Spell) {
+            this.renderSpell(graphic, entity);
         }
     }   
     
@@ -57,5 +70,15 @@ public class GamePanel extends JPanel {
         }
 
         graphic.drawImage(asset, (int)enemy.getPosition().get().getX(), (int)enemy.getPosition().get().getY(), null);
+    }
+
+    private void renderSpell(final Graphics2D graphic, final Entity spell) {
+        Image asset = null;
+        if(spell.getName().equals("fireball")) {
+            asset = this.fireballImage;
+        } else if(spell.getName().equals("arrows")) {
+            asset = this.arrowsImage;
+        }
+        graphic.drawImage(asset, (int)spell.getPosition().get().getX(), (int)spell.getPosition().get().getY(), null);
     }
 }
