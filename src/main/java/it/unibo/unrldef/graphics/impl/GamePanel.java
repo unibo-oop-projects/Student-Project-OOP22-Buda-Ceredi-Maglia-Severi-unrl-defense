b6e17b9ff28c096e4fcbdc8948c747c64c7de53f
Danilo Maglia <danilo.maglia@studio.unibo.it>
2023-03-09 22:13:29 +0100

    Added missing file to last commit

--- src/main/java/it/unibo/unrldef/graphics/impl/GamePanel.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/GamePanel.java
@@ -3,40 +3,114 @@ package it.unibo.unrldef.graphics.impl;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
 
+import it.unibo.unrldef.common.Position;
 import it.unibo.unrldef.model.api.Enemy;
 import it.unibo.unrldef.model.api.Entity;
 import it.unibo.unrldef.model.api.Spell;
 import it.unibo.unrldef.model.api.World;
-import it.unibo.unrldef.model.impl.Goblin;
-import it.unibo.unrldef.model.impl.Orc;
 
 import java.awt.Image;
 
 public class GamePanel extends JPanel {
 
     private World gameWorld;
+    private ViewState viewState;
+    
     private Image orcImage;
     private Image goblinImage;
     private Image fireballImage;
     private Image arrowsImage;
 
+    private enum ViewState {
+        IDLE,
+        TOWER_SELECTED,
+        SPELL_SELECTED
+    }
 
     public GamePanel(World gameWorld) {
         super();
+
+        this.viewState = ViewState.IDLE;
         //TODO: load assets
         try {
             this.fireballImage = ImageIO.read(new File("assets\\fireball.png"));
             this.arrowsImage = ImageIO.read(new File("assets\\arrows.png"));
+            this.orcImage = ImageIO.read(new File("assets\\orc.png"));
+            //this.goblinImage = ImageIO.read(new File("assets\\goblin.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         this.gameWorld = gameWorld;
+        
+        this.addMouseListener(new MouseInputListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Position pos = new Position(e.getX(), e.getY());
+                switch (viewState) {
+                    case IDLE:
+                        break;
+                    case TOWER_SELECTED:
+                        //TODO: send position to World
+                        break;
+                    case SPELL_SELECTED:
+                        //TODO: send position to World
+                        break;
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                return;
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                return;
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                return;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                return;
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                return;
+                
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                return;
+            }
+            
+        });
+        JButton cannon = new JButton("CANNON");
+        cannon.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewState = ViewState.TOWER_SELECTED;
+            }
+            
+        });
     }
 
     @Override
@@ -50,6 +124,17 @@ public class GamePanel extends JPanel {
         for (Entity entity : gameWorld.getSceneEntities()) {
             renderEntity(graphic, entity);
         }
+        
+        if(viewState == ViewState.TOWER_SELECTED) {
+            // TODO: get available positions from world and render them as rectangles
+            // graphic.setColor(java.awt.Color.GREEN);
+            // for (Position p: availablePosition) {
+            //     graphic.drawRect((int)p.getX(), (int)p.getY(), 50, 50);
+            //     graphic.fillRect((int)p.getX(), (int)p.getY(), 50, 50);
+            // }
+            // graphic.setColor(java.awt.Color.BLACK);
+        }
+
     }
 
     private void renderEntity(Graphics2D graphic, Entity entity) {
@@ -62,11 +147,16 @@ public class GamePanel extends JPanel {
     
     private void renderEnemy(Graphics2D graphic, Entity enemy) {
         Image asset = null;
-        
-        if(enemy instanceof Orc) {
-            asset = orcImage;
-        } else if(enemy instanceof Goblin) {
-            asset = goblinImage;
+
+        switch(enemy.getName()) {
+            case "orc":
+                asset = orcImage;
+                break;
+            case "goblin":
+                asset = goblinImage;
+                break;
+            default:
+                break;
         }
 
         graphic.drawImage(asset, (int)enemy.getPosition().get().getX(), (int)enemy.getPosition().get().getY(), null);
--- src/main/java/it/unibo/unrldef/graphics/impl/ViewImpl.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/ViewImpl.java
@@ -1,14 +1,37 @@
 package it.unibo.unrldef.graphics.impl;
 
+import javax.swing.JFrame;
+
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import it.unibo.unrldef.graphics.api.View;
 import it.unibo.unrldef.model.api.World;
 
 public class ViewImpl implements View{
 
     private final GamePanel gamePanel;
+    private final JFrame frame;
 
     public ViewImpl(World world){
-        this.gamePanel = new GamePanel(world);
+        this.frame = new JFrame("Unreal Defense");
+		this.frame.setSize(1280,720);
+		this.frame.setMinimumSize(new Dimension(1280,720));
+		this.frame.setResizable(true);
+		// frame.setUndecorated(true); // Remove title bar
+		this.gamePanel = new GamePanel(world);
+		this.frame.getContentPane().add(this.gamePanel);
+		this.frame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent ev){
+				System.exit(-1);
+			}
+			public void windowClosed(WindowEvent ev){
+				System.exit(-1);
+			}
+		});
+		this.frame.pack();
+		this.frame.setVisible(true);
     }
 
     @Override
