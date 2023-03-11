d48f7d030de124ebfbe1142d355f29cba6bae7ed
Danilo Maglia <danilo.maglia@studio.unibo.it>
2023-03-11 11:02:02 +0100

    Built the world in the main method

--- src/main/java/it/unibo/unrldef/UnrealDefense.java
+++ src/main/java/it/unibo/unrldef/UnrealDefense.java
@@ -1,11 +1,17 @@
 
 package it.unibo.unrldef;
 
+import java.util.Optional;
+
 import it.unibo.unrldef.common.Position;
 import it.unibo.unrldef.core.GameEngine;
 import it.unibo.unrldef.model.api.Path;
 import it.unibo.unrldef.model.api.Player;
 import it.unibo.unrldef.model.api.World;
+import it.unibo.unrldef.model.impl.Cannon;
+import it.unibo.unrldef.model.impl.Goblin;
+import it.unibo.unrldef.model.impl.Hunter;
+import it.unibo.unrldef.model.impl.Orc;
 import it.unibo.unrldef.model.impl.PlayerImpl;
 import it.unibo.unrldef.model.impl.WorldImpl;
 
@@ -33,10 +39,25 @@ public class UnrealDefense {
 		.addPathSegment(Path.Direction.DOWN, 8)
 		.addPathSegment(Path.Direction.LEFT, 20)
 		.addPathSegment(Path.Direction.DOWN, 3)
-
+		.addWave()
+		.addHordeToWave(0, 50000)
+		.addMultipleEnemiesToHorde(0, 0, new Orc(Optional.of(new Position(30, 0))), (short)5)
+		.addMultipleEnemiesToHorde(0, 0, new Goblin(Optional.of(new Position(30, 0))), (short)5)
+		.addAvailableTower("cannon", new Cannon(null))
+		.addAvailableTower("hunter", new Hunter(null))
+		.addTowerBuildingSpace(14, 3)
+		.addTowerBuildingSpace(24, 3)
+		.addTowerBuildingSpace(34, 4)
+		.addTowerBuildingSpace(9, 12)
+		.addTowerBuildingSpace(19, 11)
+		.addTowerBuildingSpace(3, 21)
+		.addTowerBuildingSpace(29, 21)
+		.addTowerBuildingSpace(17, 30)
+		.addTowerBuildingSpace(31, 30)
+		.addTowerBuildingSpace(23, 38)
+		.addTowerBuildingSpace(33, 38)
 		.build();
-		p.setGameMap(world);
-		engine.initGame("Regina Elisabetta dello sdrogo", world);
+		engine.initGame("io", world);
 		engine.GameLoop();
 	}
 
