334edaf4b54c2b079bd16f88667bb17b86663d33
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2023-03-29 15:11:55 +0200

    Added title logo and changed background colors

--- src/main/java/it/unibo/unrldef/graphics/impl/MenuPanel.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/MenuPanel.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,13 +22,14 @@ import it.unibo.unrldef.input.api.Input.HitType;
 
 public class MenuPanel extends JPanel {
 
-    private static final long serialVersionUID = 1L;
+    private static final String ASSETS_FOLDER = "assets" + File.separator;
     private final JButton exitButton, startButton;
     private final JTextField nameField;
     private final Input inputHandler;
     private MenuPanel panelRef;
     private double xScale = 1;
     private double yScale = 1;
+    private Image title;
     private final int DEFAULT_WIDTH = 600;
     private final int DEFAULT_HEIGHT = 400;
 
@@ -35,6 +40,12 @@ public class MenuPanel extends JPanel {
     public MenuPanel(Input inputHandler) {
         super();
         this.panelRef = this;
+        try {
+            this.title = ImageIO.read(new File(ASSETS_FOLDER + "logo.png"));
+        } catch (IOException e) {
+            new ErrorDialog("Error while loading the logo");
+        }
+
         this.addComponentListener(new ComponentListener() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -52,21 +63,21 @@ public class MenuPanel extends JPanel {
         });
         this.inputHandler = inputHandler;
         this.setLayout(null);
-        this.setBackground(new java.awt.Color(194, 148, 103));
+        this.setBackground(new java.awt.Color(255, 255, 255));
         this.nameField = new JTextField();
         this.nameField.setBounds(DEFAULT_WIDTH / 2 - 70, DEFAULT_HEIGHT / 2, 140, 20);
         this.startButton = new JButton("Start");
         this.startButton.setBounds(DEFAULT_WIDTH / 2 - 70, DEFAULT_HEIGHT / 2 + 30, 140, 20);
         this.startButton.addActionListener(e -> {
-                if (this.nameField.getText().length() > 0) {
-                    this.inputHandler.setLastHit(0, 0, HitType.START_GAME, Optional.of(this.nameField.getText()));
-                }
-            });
+            if (this.nameField.getText().length() > 0) {
+                this.inputHandler.setLastHit(0, 0, HitType.START_GAME, Optional.of(this.nameField.getText()));
+            }
+        });
         exitButton = new JButton("Exit");
         exitButton.setBounds(DEFAULT_WIDTH / 2 - 70, DEFAULT_HEIGHT / 2 + 60, 140, 20);
         exitButton.addActionListener(e -> {
-                this.inputHandler.setLastHit(0, 0, HitType.EXIT_GAME, Optional.empty());
-            });
+            this.inputHandler.setLastHit(0, 0, HitType.EXIT_GAME, Optional.empty());
+        });
         this.add(nameField);
         this.add(startButton);
         this.add(exitButton);
@@ -75,13 +86,12 @@ public class MenuPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        int width = (panelRef.getWidth()/2) - 85;
+        int width = (panelRef.getWidth()/2) - 90;
         int height = (panelRef.getHeight()/2) - 100;
-        int fontName = Math.min((int)xScale, (int)yScale) == 0 ? 20 : (int)((20) * Math.min((int)xScale, (int)yScale));
         int fontSubtitle = Math.min((int)xScale, (int)yScale) == 0 ? 15 : (int)((15) * Math.min((int)xScale, (int)yScale));
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, fontName));
-        g.drawString("UNREAL DEFENSE", width, height);
+        g.drawImage(this.title, this.getWidth()/4, this.getHeight()/10, 
+                panelRef.getWidth()/2, panelRef.getHeight()/3, null);
+        g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.BOLD, fontSubtitle));
         g.drawString("Inserisci il tuo nome:", width+15, height + 80);
         this.nameField.setBounds((int)((DEFAULT_WIDTH / 2 - 70) * xScale), (int)((DEFAULT_HEIGHT / 2) * yScale), (int)(140 * xScale), (int)(20 * yScale));
