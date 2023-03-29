7ea9797fa0baca8afb3c88c8f227484d92e611f4
Francesco Buda <francesco.buda3@studio.unibo.it>
2023-03-29 15:29:10 +0200

    added javadoc to world.java

--- src/main/java/it/unibo/unrldef/model/api/World.java
+++ src/main/java/it/unibo/unrldef/model/api/World.java
@@ -18,14 +18,66 @@ public interface World {
         DEFEAT
     }
 
+    /**
+     * @param pos the position of the tower to build
+     * @param towerName the name of the tower to build
+     * @return true if the construction of the tower was successful
+     */
     Boolean tryBuildTower(Position pos, String towerName);
+
+    /**
+     * 
+     * @return the list of the entities in the world
+     */
     List<Entity> getSceneEntities();
+
+    /**
+     * 
+     * @return the castle's integrity
+     */
     Integrity getCastleIntegrity();
+
+    /**
+     * 
+     * @return a set of the available positions
+     */
     Set<Position> getAvailablePositions();
+
+    /**
+     * 
+     * @return money in the bank
+     */
     double getMoney();
+
+    /**
+     * 
+     * @return a set of the available towers
+     */
     Set<Tower> getAvailableTowers();
+
+    /**
+     * 
+     * @param center the center of the circle
+     * @param radius the radius of the circle
+     * @return the enemies sorrounding the center
+     */
     List<Enemy> sorroundingEnemies(Position center, double radius);
+
+    /**
+     * 
+     * @return the world's path
+     */
     Path getPath();
+
+    /**
+     * 
+     * @return the state of the world
+     */
     GameState gameState();
+
+    /**
+     * update the state of the world
+     * @param time time elapsed since last update
+     */
     void updateState(long time);
 }
