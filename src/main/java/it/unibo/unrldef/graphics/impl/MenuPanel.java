26174183db06f907c87b6b3df5b6c8d9f851de21
Tommaso Ceredi <cere.cere_2001@libero.it>
2023-03-31 10:14:13 +0200

    fix merge

--- src/main/java/it/unibo/unrldef/graphics/impl/MenuPanel.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/MenuPanel.java
@@ -22,12 +22,13 @@ import it.unibo.unrldef.input.api.Input.HitType;
 
 /**
  * This class modules the menu panel.
+ * 
  * @author tommaso.ceredi@studio.unibo.it
  */
 public final class MenuPanel extends JPanel {
 
-public class MenuPanel extends JPanel {
-
+    private static final String ASSETS_FOLDER = "resources/assets" + File.separator;
+    private Image title;
     private static final long serialVersionUID = 1L;
     private final JButton exitButton, startButton;
     private final JTextField nameField;
@@ -37,16 +38,15 @@ public class MenuPanel extends JPanel {
     private double yScale = 1;
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 400;
-    private static final Color BACKGROUND_COLOR = new Color(194, 148, 103);
+    private static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
 
-    private static final int FONT_SIZE = 20;
     private static final int FONT_SUBTITLE_SIZE = 15;
     private static final int PADDING = 15;
     private static final int BUTTON_WIDTH = 140;
     private static final int BUTTON_HEIGHT = 20;
     private static final int BUTTON_PADDING = 30;
     private static final int CENTER = 70;
-    private static final Pair<Integer, Integer> TITLE_CENTER = new Pair<>(85, 100);
+    private static final Pair<Integer, Integer> TITLE_CENTER = new Pair<>(90, 80);
 
     @Override
     public Dimension getPreferredSize() {
@@ -94,12 +94,9 @@ public class MenuPanel extends JPanel {
         this.nameField = new JTextField();
         this.nameField.setBounds(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT);
         this.startButton = new JButton("Start");
-        this.startButton.setBounds(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT / 2 + BUTTON_PADDING, BUTTON_WIDTH, BUTTON_HEIGHT);
+        this.startButton.setBounds(DEFAULT_WIDTH / 2, DEFAULT_HEIGHT / 2 + BUTTON_PADDING, BUTTON_WIDTH,
+                BUTTON_HEIGHT);
         this.startButton.addActionListener(e -> {
-            if (this.nameField.getText().length() > 0) {
-                this.inputHandler.setLastHit(0, 0, HitType.START_GAME, Optional.of(this.nameField.getText()));
-            }
-        });
             if (this.nameField.getText().length() > 0) {
                 this.inputHandler.setLastHit(0, 0, HitType.START_GAME, Optional.of(this.nameField.getText()));
             }
@@ -107,8 +104,6 @@ public class MenuPanel extends JPanel {
         exitButton = new JButton("Exit");
         exitButton.setBounds(DEFAULT_HEIGHT / 2, DEFAULT_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT);
         exitButton.addActionListener(e -> {
-            this.inputHandler.setLastHit(0, 0, HitType.EXIT_GAME, Optional.empty());
-        });
             this.inputHandler.setLastHit(0, 0, HitType.EXIT_GAME, Optional.empty());
         });
         this.add(nameField);
@@ -121,16 +116,15 @@ public class MenuPanel extends JPanel {
         super.paint(g);
         final int width = panelRef.getWidth() / 2 - TITLE_CENTER.getFirst();
         final int height = panelRef.getHeight() / 2 - TITLE_CENTER.getSecond();
-        final int fontName = Math.min((int) xScale, (int) yScale) == 0 ? FONT_SIZE
-                : (int) ((FONT_SIZE) * Math.min((int) xScale, (int) yScale));
         final int fontSubtitle = Math.min((int) xScale, (int) yScale) == 0 ? FONT_SUBTITLE_SIZE
                 : (int) ((FONT_SUBTITLE_SIZE) * Math.min((int) xScale, (int) yScale));
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, fontName));
-        g.drawString("UNREAL DEFENSE", width, height);
+        g.setColor(Color.BLACK);
+        g.drawImage(this.title, this.getWidth() / 4, this.getHeight() / 10,
+                panelRef.getWidth() / 2, panelRef.getHeight() / 3, null);
         g.setFont(new Font("Arial", Font.BOLD, fontSubtitle));
         g.drawString("Inserisci il tuo nome:", width + PADDING, height + CENTER);
-        this.nameField.setBounds((int) ((DEFAULT_WIDTH / 2 - CENTER) * xScale), (int) ((DEFAULT_HEIGHT / 2) * yScale),
+        this.nameField.setBounds((int) ((DEFAULT_WIDTH / 2 - CENTER) * xScale),
+                (int) ((DEFAULT_HEIGHT / 2) * yScale),
                 (int) (BUTTON_WIDTH * xScale), (int) (BUTTON_HEIGHT * yScale));
         this.startButton.setBounds((int) ((DEFAULT_WIDTH / 2 - CENTER) * xScale),
                 (int) ((DEFAULT_HEIGHT / 2 + BUTTON_PADDING) * yScale), (int) (BUTTON_WIDTH * xScale),
@@ -144,5 +138,4 @@ public class MenuPanel extends JPanel {
             this.startButton.setEnabled(false);
         }
     }
-
 }
