2dd54275de0a6cf6d5cb5937c77cb974ccad0a93
Francesco Buda <francesco.buda3@studio.unibo.it>
2023-03-31 18:14:16 +0200

    added some javadoc and corrections

--- src/main/java/it/unibo/unrldef/graphics/impl/SpriteLoader.java
+++ src/main/java/it/unibo/unrldef/graphics/impl/SpriteLoader.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import java.awt.Image;
-
 import javax.imageio.ImageIO;
 
 import org.json.simple.JSONArray;
@@ -17,49 +16,116 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * Class that contains the dimensions of the sprites in the game.
+ * Class that loads from file and contains the data of the sprites.
+ * 
  * @author danilo.maglia@studio.unibo.it
+ * @author francesco.buda3@studio.unibo.it
  */
 public class SpriteLoader {
-    public final Map<String, Sprite> sprites = new HashMap<>();
+    /**
+     * the folder where the assets are stored.
+     */
     private static final String ASSETS_FOLDER = "resources/assets" + File.separator;
+    /**
+     * the name of the sprite that represents the first map.
+     */
+    public static final String FIRST_MAP = "firstMap";
+    /**
+     * the name of the sprite that represents the fire ball.
+     */
+    public static final String FIRE_BALL = "fireBall";
+    /**
+     * the name of the sprite that represents the fire tower.
+     */
+    public static final String SNOW_STORM = "snowStorm";
+    /**
+     * the name of the sprite that represents the orc.
+     */
+    public static final String ORC = "orc";
+    /**
+     * the name of the sprite that represents the goblin.
+     */
+    public static final String GOBLIN = "goblin";
+    /**
+     * the name of the sprite that represents the hunter.
+     */
+    public static final String HUNTER = "hunter";
+    /**
+     * the name of the sprite that represents the cannon.
+     */
+    public static final String CANNON = "cannon";
+    /**
+     * the name of the sprite that represents the shooting hunter.
+     */
+    public static final String SHOOTING_HUNTER = "shootingHunter";
+    /**
+     * the name of the sprite that represents the shooting cannon.
+     */
+    public static final String SHOOTING_CANNON = "shootingCannon";
+    /**
+     * the name of the sprite that represents the explosion.
+     */
+    public static final String EXPLOSION = "explosion";
+    /**
+     * the name of the sprite that represents the tower place.
+     */
+    public static final String TOWER_PLACE = "towerPlace";
+    /**
+     * the name of the sprite that represents the heart.
+     */
+    public static final String HEART = "heart";
+    /**
+     * the name of the sprite that represents the money.
+     */
+    public static final String MONEY = "money";
 
-    public void loadSpritesFromFile (String fileName) {
-        JSONParser parser = new JSONParser();
-        String fileContent = "";
+    private final Map<String, Sprite> sprites = new HashMap<>();
+
+    /**
+     * loads the sprites from the JSON file passed as argument.
+     * 
+     * @param fileName
+     */
+    public void loadSpritesFromFile(final String fileName) {
+        final JSONParser parser = new JSONParser();
+        String fileContent;
         JSONObject json = new JSONObject();
 
         try {
-			// read the whole file passed as argument and put the content in a string
-			fileContent = new String(Files.readAllBytes(Paths.get(fileName)));
-			json = (JSONObject) parser.parse(fileContent);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+            // read the whole file passed as argument and put the content in a string
+            fileContent = new String(Files.readAllBytes(Paths.get(fileName)));
+            json = (JSONObject) parser.parse(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace(); //NOPMD
+        } catch (ParseException e) {
+            e.printStackTrace(); //NOPMD
+        }
 
-        JSONArray sprites = (JSONArray) json.get("sprites");
+        final JSONArray sprites = (JSONArray) json.get("sprites");
 
         // for each sprite in the json file, create a new sprite and put it in the map
-        for (Object s : sprites) {
-            JSONObject sprite = (JSONObject) s;
-            int width = Integer.parseInt(sprite.get("width").toString());
-            int height = Integer.parseInt(sprite.get("height").toString());
+        for (final Object s : sprites) {
+            final JSONObject sprite = (JSONObject) s;
+            final int width = Integer.parseInt(sprite.get("width").toString());
+            final int height = Integer.parseInt(sprite.get("height").toString());
             Image spriteImage = null;
             try {
                 spriteImage = ImageIO.read(new File(ASSETS_FOLDER + sprite.get("fileName").toString()));
             } catch (IOException e) {
-                e.printStackTrace();
+                e.printStackTrace(); //NOPMD
             }
-            Sprite newSprite = new Sprite(width, height, spriteImage);
+            final Sprite newSprite = new Sprite(width, height, spriteImage);
             this.sprites.put(sprite.get("name").toString(), newSprite);
         }
 
     }
-    
-    //returns the sprite given its name
-    public Sprite getSprite(String name) {
+
+    /**
+     * 
+     * @param name
+     * @return the sprite with the given name
+     */
+    public Sprite getSprite(final String name) {
         return this.sprites.get(name);
     }
 }
