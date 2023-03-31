54e23b946c8db995143cdc87487f213911668378
Francesco Buda <francesco.buda3@studio.unibo.it>
2023-03-31 18:29:57 +0200

    added some javadoc and corrections

--- src/test/java/it/unibo/unrldef/model/impl/WorldImplTest.java
+++ src/test/java/it/unibo/unrldef/model/impl/WorldImplTest.java
@@ -9,10 +9,16 @@ import it.unibo.unrldef.common.Position;
 import it.unibo.unrldef.model.api.Enemy;
 import it.unibo.unrldef.model.api.World;
 
+/**
+ * Test class for WorldImpl.
+ */
 public class WorldImplTest {
 
     private World testWorld;
 
+    /**
+     * Initialize the world.
+     */
     @BeforeEach
     public void init() {
         final LevelBuilder testLevel = new LevelBuilder(new PlayerImpl());
@@ -21,12 +27,25 @@ public class WorldImplTest {
 
     @Test
     void testEnemiesInRange() {
-        Position testPos1 = new Position(14, 14);
-        Position testPos2 = new Position(10, 24);
-        Position testPos3 = new Position(4, 4);
-        Position testPos4 = new Position(16, 34);
-        Position testPos5 = new Position(34, 68);
-        Position testPos6 = new Position(18, 24);
+        final int testX1 = 14;
+        final int testY1 = 14;
+        final int testX2 = 10;
+        final int testY2 = 24;
+        final int testX3 = 4;
+        final int testY3 = 4;
+        final int testX4 = 16;
+        final int testY4 = 34;
+        final int testX5 = 34;
+        final int testY5 = 68;
+        final int testX6 = 18;
+        final int testY6 = 24;
+
+        Position testPos1 = new Position(testX1, testY1);
+        Position testPos2 = new Position(testX2, testY2);
+        Position testPos3 = new Position(testX3, testY3);
+        Position testPos4 = new Position(testX4, testY4);
+        Position testPos5 = new Position(testX5, testY5);
+        Position testPos6 = new Position(testX6, testY6);
 
         Enemy testEnemy1 = new Orc();
         Enemy testEnemy2 = new Orc();
@@ -34,17 +53,19 @@ public class WorldImplTest {
         Enemy testEnemy4 = new Goblin();
         Enemy testEnemy5 = new Goblin();
 
+        final int testRadius = 14;
+
         this.testWorld.spawnEnemy(testEnemy1, testPos1); //spawning the first enemy in the path
         this.testWorld.spawnEnemy(testEnemy2, testPos2); //spawning the second enemy in the path
         this.testWorld.spawnEnemy(testEnemy3, testPos3); //spawning the third enemy out of the path
         this.testWorld.spawnEnemy(testEnemy4, testPos4); //spawning the fourth enemy in the path and more advanced than the others
         this.testWorld.spawnEnemy(testEnemy5, testPos5); //spawning the fifth enemy in the path, but too far 
 
-        var sorroundingEnemies = this.testWorld.sorroundingEnemies(testPos6, 14);
+        var sorroundingEnemies = this.testWorld.sorroundingEnemies(testPos6, testRadius);
 
-        assert(sorroundingEnemies.containsAll(List.of(testEnemy1, testEnemy2, testEnemy4)));
-        assert(!sorroundingEnemies.contains(testEnemy3));
-        assert(!sorroundingEnemies.contains(testEnemy5));
-        assert(sorroundingEnemies.get(0).equals(testEnemy4));
+        assert (sorroundingEnemies.containsAll(List.of(testEnemy1, testEnemy2, testEnemy4)));
+        assert (!sorroundingEnemies.contains(testEnemy3));
+        assert (!sorroundingEnemies.contains(testEnemy5));
+        assert (sorroundingEnemies.get(0).equals(testEnemy4));
     }
 }
